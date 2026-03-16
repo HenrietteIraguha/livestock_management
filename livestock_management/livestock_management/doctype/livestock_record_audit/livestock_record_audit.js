cfd9227d937ed783daeb5782a44386b6582bb34e
HenrietteIraguha <h.iraguha@alustudent.com>
2026-03-16 21:10:56 +0000

    Phase 2: all DocTypes complete - Livestock, Treatment, Feeding, Settings, Audit

---
+++ livestock_management/livestock_management/doctype/livestock_record_audit/livestock_record_audit.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Henriette and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Livestock Record Audit", {
+// 	refresh(frm) {
+
+// 	},
+// });
